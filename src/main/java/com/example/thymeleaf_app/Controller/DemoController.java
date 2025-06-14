1ca4b419e42f2826d47259fb8cacb26bd7583942
Sandu Guzun <sanduguzun762@gmail.com>
2025-06-14 14:36:35 +0300

    Chore: Add file html in templates

---
+++ src/main/java/com/example/thymeleaf_app/Controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.example.thymeleaf_app.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class DemoController {
+
+    // aici vom crea mapping-up pentur "/hello"
+    @GetMapping("/hello")
+    public String sayHello(Model theModel) {
+
+        theModel.addAttribute("theDate", java.time.LocalDateTime.now());
+
+        return "helloworld";
+    }
+
+}
